83019a3a1b141d0a823bf47128b0a0d674224c73
parixitsoni <parikshitsoni85@gmail.com>
2020-06-28 18:48:50 +0530

    Update worker.js
    add changes

--- public/worker.js
+++ public/worker.js
@@ -3,45 +3,50 @@ var urlsToCache = [
   '/'
 ];
 
-// Install a service worker
-self.addEventListener('install', event => {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
+self.addEventListener("install", (e) => {
+    self.skipWaiting();
+    e.waitUntil(
+      caches.open(appVersion).then((cache) => {
+        return cache
+          .addAll(files)
+          .then(() => console.log("App Version: " + appVersion))
+          .catch((err) => console.error("Error adding files to cache", err));
       })
-  );
-});
-
-// Cache and return requests
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
-
-// Update a service worker
-self.addEventListener('activate', event => {
-  var cacheWhitelist = ['pwa-task-manager'];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+    );
+  });
+  self.addEventListener("activate", (e) => {
+    e.waitUntil(
+      caches.keys().then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cache) => {
+            if (cache !== appVersion) {
+              console.info("Deleting Old Cache", cache);
+              return caches.delete(cache);
+            }
+          })
+        );
+      })
+    );
+    return self.clients.claim();
+  });
+  self.addEventListener("fetch", (e) => {
+    const req = e.request;
+    const url = new URL(req.url);
+    if (url.origin === location.origin) return e.respondWith(cacheFirst(req));
+    else return e.respondWith(networkFirst(req));
+  });
+  async function cacheFirst(req) {
+    let cacheRes = await caches.match(req);
+    return cacheRes || fetch(req);
+  }
+  async function networkFirst(req) {
+    const dynamicCache = await caches.open("dynamic");
+    try {
+      const networkResponse = await fetch(req);
+      if (req.method !== "POST") dynamicCache.put(req, networkResponse.clone());
+      return networkResponse;
+    } catch (err) {
+      const cacheResponse = await caches.match(req);
+      return cacheResponse;
+    }
+  }
