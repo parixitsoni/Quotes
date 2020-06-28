bb042c9cfe1add4eec8a6409e8e2a328f1e987b4
parixitsoni <parikshitsoni85@gmail.com>
2020-06-28 18:10:47 +0530

    add build folder through cmd

---
+++ build/precache-manifest.70e0c7a64cf3faa3f775e4a959f22cf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0e2efd7869da4d8e5cf54d95cf8ac7a",
+    "url": "/quotes/index.html"
+  },
+  {
+    "revision": "df299ad0d7ad920ea160",
+    "url": "/quotes/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "dc2df7795a19b5131cba",
+    "url": "/quotes/static/css/main.d511451e.chunk.css"
+  },
+  {
+    "revision": "df299ad0d7ad920ea160",
+    "url": "/quotes/static/js/2.bd407fc5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/quotes/static/js/2.bd407fc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc2df7795a19b5131cba",
+    "url": "/quotes/static/js/main.9212e28e.chunk.js"
+  },
+  {
+    "revision": "4de52a8d5a5fbd9ce26d",
+    "url": "/quotes/static/js/runtime-main.96cfa0ba.js"
+  }
+]);
